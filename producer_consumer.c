fafff526f0c3b6f00953a6f7a46a30aaea45512d
Jorge Loch <jorgembloch2001@gmail.com>
2024-09-12 22:01:42 -0300

    fix: change formatting to the print_clock function

--- producer_consumer.c
+++ producer_consumer.c
@@ -172,9 +172,9 @@ void* producerBehavior(void *producer_args) {
 }
 
 void print_clock(Clock c) {
-  printf("clock: ( %i", c.c[0]);
+  printf("clock: (%i", c.c[0]);
   for (int i = 1; i < THREAD_NUM; i++) {
-    printf(", %i", c.c[i]);
+    printf(",%i", c.c[i]);
   }
   printf(")\n");
 }
